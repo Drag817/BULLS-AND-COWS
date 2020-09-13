899860e064589966a4808f093ea7dbbf2556d2da
kostya2718 <kostik19141@mail.ru>
2020-09-13 13:51:35 +0700

    Create header file

---
+++ src/functions.h
@@ -0,0 +1,15 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+void swap(int& x, int& y);
+void Random(int* A, int N);
+void AnswerCheck(int* A, int* B, int& bull, int& cow, int N);
+int Menu();
+
+#endif
